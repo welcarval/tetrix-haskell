db225b31226d4cff6570a46d798c2ecb27616ab0
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-06-21 18:36:46 -0300

    feat: use a state to keep values between paintings

--- src/Main.hs
+++ src/Main.hs
@@ -5,23 +5,33 @@ import Graphics.Gloss.Interface.Pure.Game
 windowDisplay :: Display
 windowDisplay = InWindow "Tetrix" (400, 600) (800, 200)
 
-type World = Float
+data GameState = GameState {
+    stepAcc :: Float,
+    posY :: Float
+}
 
 main :: IO ()
 main = play
     windowDisplay
     black
     60
-    0.0
+    GameState {stepAcc=0, posY=0}
     render
     handleEvent
     step
 
-render :: World -> Picture
-render world = translate 0 (world - 10) $ color (makeColorI 10 178 10 200) $ rectangleSolid 20 20
+render :: GameState -> Picture
+render state = translate 0 (posY state - 10) $ color (makeColorI 10 178 10 200) $ rectangleSolid 20 20
 
-handleEvent :: Event -> World -> World
-handleEvent _ world = world
+handleEvent :: Event -> GameState -> GameState
+handleEvent _ state = state
 
-step :: Float -> World -> World
-step dt world = if dt `mod` 60.0 == 0 && world > -280 then world - 20 else world
+step :: Float -> GameState -> GameState 
+step _ state = if stepAcc state == 60 && posY state > -280 
+               then state {
+                          stepAcc = 0, 
+                          posY = posY state - 20
+                          } 
+               else state {
+                          stepAcc = stepAcc state + 1
+                          }
