028187ae68bf56079110d8bd7e779f806e615c7a
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-08-05 10:39:29 -0300

    feat: when holding d, rightKey or leftKey, the state keeps the movement to the next step

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,10 @@
 module Main (main) where
-import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
-import Graphics.Gloss.Juicy
-import System.Random (StdGen, newStdGen)
-import TetrixBoard
-import TetrixPiece
+import           Graphics.Gloss
+import           Graphics.Gloss.Interface.Pure.Game
+import           Graphics.Gloss.Juicy
+import           System.Random                      (StdGen, newStdGen)
+import           TetrixBoard
+import           TetrixPiece
 
 windowWidth :: Int
 windowWidth = 900
@@ -24,40 +24,60 @@ data Assets = Assets {
     _startGameAsset :: Picture
 }
 
+data RepeatableKey = RepeatLeft | RepeatRight | RepeatSoftDrop deriving Eq
+
+toRepeatableKey :: Key -> Maybe RepeatableKey
+toRepeatableKey (SpecialKey KeyLeft)  = Just RepeatLeft
+toRepeatableKey (SpecialKey KeyRight) = Just RepeatRight
+toRepeatableKey (Char 'd')            = Just RepeatSoftDrop
+toRepeatableKey _                     = Nothing
+
+fromRepeatableKey :: RepeatableKey -> Key
+fromRepeatableKey RepeatLeft     = SpecialKey KeyLeft
+fromRepeatableKey RepeatRight    = SpecialKey KeyRight
+fromRepeatableKey RepeatSoftDrop = Char 'd'
+
+keyRepeatInterval :: Float
+keyRepeatInterval = 0.05
+
 data TetrixWindow = TetrixWindow {
-    _board        :: SomeTetrixBoard,
-    _nextPieceL   :: Picture,
-    _levelLabel   :: Picture,
-    _scoreLabel   :: Picture,
-    _controls     :: Picture,
-    _scoreDisplay :: Picture,
-    _levelDisplay :: Picture,
-    _linesDisplay :: Picture,
-    _startButton  :: Picture,
-    _quitButton   :: Picture,
-    _pauseButton  :: Picture,
-    _stdGen       :: StdGen
+    _board                :: SomeTetrixBoard,
+    _nextPieceL           :: Picture,
+    _levelLabel           :: Picture,
+    _scoreLabel           :: Picture,
+    _controls             :: Picture,
+    _scoreDisplay         :: Picture,
+    _levelDisplay         :: Picture,
+    _linesDisplay         :: Picture,
+    _startButton          :: Picture,
+    _quitButton           :: Picture,
+    _pauseButton          :: Picture,
+    _heldKey              :: Maybe RepeatableKey,
+    _keyRepeatableElapsed :: Float,
+    _stdGen               :: StdGen
 }
 
 createWindow :: StdGen -> Assets -> TetrixWindow
 createWindow g assets =
     TetrixWindow {
-        _board        = SomeTetrixBoard SCreated $ createBoard g boardLabels,
-        _nextPieceL   = _nextPieceAsset assets,
-        _levelLabel   = _levelAsset assets,
-        _scoreLabel   = _scoreAsset assets,
-        _controls     = _controlsAsset assets,
-        _scoreDisplay = text "0",
-        _levelDisplay = text "0",
-        _linesDisplay = text "0",
-        _startButton  = text "start",
-        _quitButton   = text "quit",
-        _pauseButton  = text "pause",
-        _stdGen       = g
+        _board                = SomeTetrixBoard SCreated $ createBoard g boardLabels,
+        _nextPieceL           = _nextPieceAsset assets,
+        _levelLabel           = _levelAsset assets,
+        _scoreLabel           = _scoreAsset assets,
+        _controls             = _controlsAsset assets,
+        _scoreDisplay         = text "0",
+        _levelDisplay         = text "0",
+        _linesDisplay         = text "0",
+        _startButton          = text "start",
+        _quitButton           = text "quit",
+        _pauseButton          = text "pause",
+        _heldKey              = Nothing,
+        _keyRepeatableElapsed = 0,
+        _stdGen               = g
     }
     where boardLabels = BoardLabels {
-            _overlayStartGame = _startGameAsset assets, 
-            _overlayPaused    = _pausedAsset assets, 
+            _overlayStartGame = _startGameAsset assets,
+            _overlayPaused    = _pausedAsset assets,
             _overlayGameOver  = _gameOverAsset assets
         }
 
@@ -101,10 +121,23 @@ paintBoard window someBoard@(SomeTetrixBoard _ board) = finalPicture
                                          nextPieceLabelShape | i <- [0..3]]
 
 handlerEvent :: Event -> TetrixWindow -> TetrixWindow
-handlerEvent e window = finalWindow
-    where
-        board = _board window
-        finalWindow = window { _board = (keyPressEvent e board) }
+handlerEvent e@(EventKey key Down _ _) window =
+    window {
+        _board = keyPressEvent e (_board window),
+        _heldKey = case toRepeatableKey key of
+            Just rk -> Just rk
+            Nothing -> _heldKey window,
+        _keyRepeatableElapsed = case toRepeatableKey key of
+            Just _  -> 0
+            Nothing -> _keyRepeatableElapsed window
+    }
+handlerEvent (EventKey key Up _ _) window =
+    window {
+        _heldKey = if toRepeatableKey key == _heldKey window
+            then Nothing
+            else _heldKey window
+    }
+handlerEvent _ window = window
 
 
 main :: IO ()
@@ -139,4 +172,13 @@ main = do
         step
 
 step :: Float -> TetrixWindow -> TetrixWindow
-step _ window = window { _board = advanceTimer (_board window)}
+step dt window = windowAfterTimer { _keyRepeatableElapsed = newElapsed, _board = newBoard }
+    where
+        windowAfterTimer = window { _board = advanceTimer (_board window) }
+        elapsed = _keyRepeatableElapsed windowAfterTimer + dt
+
+        (newElapsed, newBoard) =
+            case _heldKey windowAfterTimer of
+                Just rk | elapsed >= keyRepeatInterval -> (0, keyPressEvent (repeatEvent rk) (_board windowAfterTimer))
+                _                                      -> (elapsed, _board windowAfterTimer)
+        repeatEvent rk = EventKey (fromRepeatableKey rk) Down (Modifiers Up Up Up) (0, 0)
